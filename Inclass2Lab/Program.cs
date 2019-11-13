baf2cdb1d178507154fa57db98be54cb40baf239
mingwang168 <mingwang0904@gmail.com>
2019-11-13 11:57:23 -0800

    Add project files.

---
+++ Inclass2Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inclass2Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
